65364861512c2046de2500c4bb3129abaefee4dc
aponceha <aponceha@gmail.com>
2023-01-24 15:19:50 -0500

    skeleton commit

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS wedevz_db;
+
+CREATE DATABASE wedevz_db;
---
+++ models/index.js
@@ -0,0 +1,32 @@
+const User = require('./User');
+const Project = require('./Project');
+const Technology = require('./Technology');
+
+User.hasMany(Project, {
+  foreignKey: 'project_id',
+});
+
+Project.belongsTo(User, {
+  foreignKey: 'project_id',
+});
+// Many to Many user to technology
+
+User.hasMany(Technology, {
+    foreignKey: 'technology_id',
+  });
+  
+Technology.belongsToMany(User, {
+    foreignKey: 'technology_id',
+  });
+
+// Many to Many Project to technology
+
+Project.hasMany(Technology, {
+    foreignKey: 'technology_id',
+  });
+  
+Technology.belongsTo(Project, {
+    foreignKey: 'technology_id',
+  });
+
+module.exports = { User, Project, Technology };
