d233f8aae17a3370211833b20b9cc9239462bdec
aponceha <114960304+aponceha@users.noreply.github.com>
2023-01-25 11:09:41 -0500

    Update Project.js

---
+++ models/Project.js
@@ -0,0 +1,62 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+const { Project } = require('.');
+
+Project.init(
+{
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [2]
+        }
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [1]
+        }
+    },
+    link: {
+        type: DataTypes.INTEGER,
+    },
+    technology: {
+        type:DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [1]
+        }
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        referece: {
+            model: 'user',
+            key: 'id'
+        }
+    },
+    technology_id: {
+        type: DataTypes.INTEGER,
+        referece: {
+            model: 'technology',
+            key: 'id'
+        }
+    },
+},
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'project',
+    }
+);
+
+module.exports = Project;
