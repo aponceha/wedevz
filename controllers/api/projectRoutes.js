2adbb72b3709c8f4bf912e997e22f39e93a1279a
Robert Alexander Iatan <robertiatan@gmail.com>
2023-01-26 21:32:17 -0500

    routes and other stuff

---
+++ controllers/api/projectRoutes.js
@@ -0,0 +1,101 @@
+const router = require("express").Router();
+const { Project, User, Technology } = require("../../models");
+
+// GET all projects
+router.get("/", (req, res) => {
+  Project.findAll()
+    .then((dbProjectData) => res.json(dbProjectData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// GET a single project
+router.get("/:id", (req, res) => {
+  Project.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbProjectData) => {
+      if (!dbProjectData) {
+        res.status(404).json({ message: "No project found with this id" });
+        return;
+      }
+      res.json(dbProjectData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// POST a new project
+router.post("/", (req, res) => {
+  Project.create({
+    name: req.body.name,
+    description: req.body.description,
+    link: req.body.link,
+    technology: req.body.technology,
+    user_id: req.body.user_id,
+    technology_id: req.body.technology_id,
+  })
+    .then((dbProjectData) => res.json(dbProjectData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// UPDATE a project
+router.put("/:id", (req, res) => {
+  Project.update(
+    {
+      name: req.body.name,
+      description: req.body.description,
+      link: req.body.link,
+      technology: req.body.technology,
+      user_id: req.body.user_id,
+      technology_id: req.body.technology_id,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbProjectData) => {
+      if (!dbProjectData) {
+        res.status(404).json({ message: "No project found with this id" });
+        return;
+      }
+      res.json(dbProjectData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// DELETE a project
+router.delete("/:id", (req, res) => {
+  Project.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbProjectData) => {
+      if (!dbProjectData) {
+        res.status(404).json({ message: "No project found with this id" });
+        return;
+      }
+      res.json(dbProjectData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
---
+++ controllers/home-routes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+
+// Render login page
+res.render("login");
+
+
+// Render signup page
+res.render("signup");
+
+// Render profile page
+res.render("profile");
+
+// Render edit page
+res.render("edit");
+
+// Render explore page
+res.render("explore");
