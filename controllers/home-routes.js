e6ebc06c274b325d44472e7a52b30a6df8a0ec16
aponceha <aponceha@gmail.com>
2023-01-30 21:00:59 -0500

    jan30 commit

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,18 +1,95 @@
 const router = require('express').Router();
+const User = require('../models/User');
+const Project = require('../models/Project');
+const Technology = require('../models/Technology');
+// const cloudinary = require('/utils/cloudinary');
+
+// Cloudinary upload
+
+
+
+
+
+
+
+
+
+
 
 
 // Render login page
-res.render("login");
+// res.render("login");
 
 
 // Render signup page
-res.render("signup");
+// res.render("signup");
 
-// Render profile page
-res.render("profile");
+// // Render profile page
+// res.render("profile");
 
-// Render edit page
-res.render("edit");
+// // Render edit page
+// res.render("edit");
 
 // Render explore page
-res.render("explore");
+
+// GET all users for explore page
+router.get('/', async (req, res) => {
+    try {
+        const userData = await User.findAll({
+            include: [
+                {
+                    model: Project,
+                    attributes: ['name', 'description', 'link', 'technology'],
+                },
+                {
+                    model: Technology,
+                    attributes: ['name'],
+                },
+            ],
+        });
+        const usergallery = userData.map((user) => 
+            user.get({ plain: true })
+        );
+        console.log(usergallery);
+        // Render Explore Page
+        res.render("explore", {
+            usergallery,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// GET all projects for explore page
+router.get('/', async (req, res) => {
+    try {
+        const projectData = await Project.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['first_name', 'last_name', 'city'],
+                },
+                {
+                    model: Technology,
+                    attributes: ['name'],
+                },
+            ],
+        });
+        const projectgallery = projectData.map((project) => 
+            project.get({ plain: true })
+        );
+        // Render Explore Page
+        res.render("explore", {
+            projectgallery,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+module.exports = router;
+
+
