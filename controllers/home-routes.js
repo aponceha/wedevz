994a9a66c23dc043652f2e01d52d379b9536d2fb
aponceha <aponceha@gmail.com>
2023-01-31 21:21:36 -0500

    jan31-9.21pm

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -33,7 +33,7 @@ const Project = require('../models/Project');
 // Render explore page
 
 // GET all users for explore page
-router.get('/', async (req, res) => {
+router.get('/explore', async (req, res) => {
     try {
         const userData = await User.findAll({
             include: [
@@ -44,12 +44,14 @@ router.get('/', async (req, res) => {
                
             ],
         });
+
+        // randomization of array
         const usergallery = userData.map((user) => 
             user.get({ plain: true })
         );
         console.log(usergallery);
         // Render Explore Page
-        res.render("explore", {
+        res.render(".", {
             usergallery,
             loggedIn: req.session.loggedIn,
         });
@@ -59,31 +61,5 @@ router.get('/', async (req, res) => {
     }
 });
 
-// GET all projects for explore page
-router.get('/', async (req, res) => {
-    try {
-        const projectData = await Project.findAll({
-            include: [
-                {
-                    model: User,
-                    attributes: ['first_name', 'last_name', 'city'],
-                },
-                
-            ],
-        });
-        const projectgallery = projectData.map((project) => 
-            project.get({ plain: true })
-        );
-        // Render Explore Page
-        res.render("explore", {
-            projectgallery,
-            loggedIn: req.session.loggedIn,
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
-});
-module.exports = router;
 
 
