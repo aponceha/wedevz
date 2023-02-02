f2f1da36717d47cefa44703fd66b5846a865118c
Robert Alexander Iatan <robertiatan@gmail.com>
2023-02-01 22:35:42 -0500

    added JAWSDB

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const sequelize = require('./config/connection');
 const helpers = require('./utils/helper');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: 'Super secret secret',
